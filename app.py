afb990d80e71bdf4144e00f490c2eae9e8bb9928
Atharva <atharvakhangar14@gmail.com>
2023-04-20 19:27:05 +0530

    Minor changes

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def login():
             session['name'] = user['name']
             session['email'] = user['email']
             mesage = 'Logged in successfully !'
-            return render_template('donate.html', mesage = mesage)
+            return render_template('index.html', mesage = mesage)
         else:
             mesage = 'Please enter correct email / password !'
     return render_template('login.html', mesage = mesage)
